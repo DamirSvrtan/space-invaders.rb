8ff5231f448aea8814ba34660ef8fd6927a560bd
Damir Svrtan <damir.svrtan@gmail.com>
2014-05-12 23:22:50 +0200

    Dynamic Images methods.

--- lib/space_invaders/images.rb
+++ lib/space_invaders/images.rb
@@ -1,63 +1,40 @@
 require_relative 'base'
+require_relative 'utils'
 
 module SpaceInvaders
   class Images < Base
 
-    def invader_a1_image
-      @invader_a1_image ||= Gosu::Image.new app, "assets/images/InvaderA1.png"
-    end
-  
-    def invader_a2_image
-      @invader_a2_image ||= Gosu::Image.new app, "assets/images/InvaderA2.png"
+    IMAGES = [ :invader_a1, 
+               :invader_a2, 
+               :invader_b1, 
+               :invader_b2, 
+               :invader_c1, 
+               :invader_c2,
+               :red_invader,
+               :ship,
+               :ship_crushed_left,
+               :ship_crushed_right,
+               :bullet,
+               :full_block,
+               :ok_block,
+               :weak_block
+             ]
+
+    IMAGES.each do |image|
+      method_name = "#{image}_image"
+      define_method method_name do
+        if instance_variable_get("@#{method_name}").nil?
+          instance_variable_set("@#{method_name}", Gosu::Image.new(app, asset_path(image)))
+        end
+        instance_variable_get("@#{method_name}")
+      end
     end
 
-    def invader_b1_image
-      @invader_b1_image ||= Gosu::Image.new app, "assets/images/InvaderB1.png"
-    end
-  
-    def invader_b2_image
-      @invader_b2_image ||= Gosu::Image.new app, "assets/images/InvaderB2.png"
-    end
+    private
 
-    def invader_c1_image
-      @invader_c1_image ||= Gosu::Image.new app, "assets/images/InvaderC1.png"
-    end
-  
-    def invader_c2_image
-      @invader_c2_image ||= Gosu::Image.new app, "assets/images/InvaderC2.png"
-    end
-
-    def red_invader_image
-      @invader_d_image ||= Gosu::Image.new app, "assets/images/InvaderD.png"
-    end
-
-    def ship_image
-      @ship_image ||= Gosu::Image.new app, "assets/images/Ship3.png"
-    end
-
-    def ship_crushed_left_image
-      @ship_crushed_left_image ||= Gosu::Image.new app, "assets/images/ShipLeft.png"
-    end
-
-    def ship_crushed_right_image
-      @ship_crushed_right_image ||= Gosu::Image.new app, "assets/images/ShipRight.png"
-    end
-
-    def bullet_image
-      @bullet_image ||= Gosu::Image.new app, "assets/images/Bullet.png"
-    end
-
-    def full_block_image
-      @full_block_image ||= Gosu::Image.new app, "assets/images/FullBlock.png"
-    end
-
-    def ok_block_image
-      @ok_block_image ||= Gosu::Image.new app, "assets/images/OkBlock.png"
-    end
-
-    def weak_block_image
-      @weak_block_image ||= Gosu::Image.new app, "assets/images/WeakBlock.png"
-    end
-
+      def asset_path(image_name)
+        camel_case_name = Utils.camelcase(image_name)
+        File.join("assets", "images", "#{camel_case_name}.png")
+      end
   end
 end
--- lib/space_invaders/utils.rb
+++ lib/space_invaders/utils.rb
@@ -1,8 +1,13 @@
 module SpaceInvaders
   module Utils
     def self.to_klass(property)
-      klass_name = property.to_s.split('_').map{|e| e.capitalize}.join
+      klass_name = camelcase(property)
       Object.const_get("SpaceInvaders::#{klass_name}")
     end
+
+    def self.camelcase(property)
+      property.to_s.split('_').map{|e| e.capitalize}.join
+    end
+
   end
 end
